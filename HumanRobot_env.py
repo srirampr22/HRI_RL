4573d709f5d3ecad792bbf1cf98695a96cb2e3ed
srirampr22 <srirampr@umich.edu>
2024-07-11 18:47:09 -0400

    Modifed observation space attributes, and normalized both action space and obs space

--- HumanRobot_env.py
+++ HumanRobot_env.py
@@ -7,13 +7,31 @@ from Human_SF import Human_SF
 from Robot import Robot
 from RobotDiff import RobotDiff
 from Rewards import RewardCalculator
+from scipy.spatial import KDTree
 
+def get_obstacle_points(robot_pos, obstacles, N):
+    """ Get the N closest points to the robot from the obstacles """
+    all_points = []
+    for line in obstacles:
+        all_points.extend(line)
+    
+    all_points = np.array(all_points)
+
+    # Build KDTree
+    kdtree = KDTree(all_points)
+    distances, indices = kdtree.query(robot_pos, k=N)
+    
+    # return the N closest points as an array of shape (N, 2)
+    closest_points = all_points[indices]
+    
+    return closest_points
 
 class HumanRobotEnv(gym.Env):
     # metadata = {'render.modes': ['human']}
     def __init__(self, seed=None, options=None, reward_weights=None):
         super(HumanRobotEnv, self).__init__()
 
+        ############### Configurations ################
         if reward_weights is None:
             reward_weights = {
                 "translation": 1.0,
@@ -22,16 +40,16 @@ class HumanRobotEnv(gym.Env):
                 "collision": 1.0,
                 "goal": 1.0,
             }
-
-        print("Reward Weights from env script: ", reward_weights)
         
         self.trans_wei = reward_weights["translation"]
         self.rot_wei = reward_weights["rotation"]
         self.vel_wei = reward_weights["velocity"]
         self.col_wei = reward_weights["collision"]
         self.goal_wei = reward_weights["goal"]
-        # add obstacles here
+        
+        # Define Obstacles and parameters here
         self.obs = [[5, -5, -1.3, -0.3], [7, 10, 5, 2]]
+        self.n_obs_points = 0
 
         self.env_size = 20.0
         self.agent_radius = [0.35, 0.35]  # Robot and Human radius
@@ -42,36 +60,48 @@ class HumanRobotEnv(gym.Env):
             "wheel_base": 0.5,
             "desired_dist": self.desired_distance,
             "robot_radius": self.agent_radius[0],
-        }
-
-        # defining action space (continuous or discrete) the actions that the agent can take is in the form of wheel velocity (v_left, v_right)
-        self.action_space = spaces.Box(
-            low=np.array([0.0, 0.0]), high=np.array([5.0, 5.0]), dtype=np.float32
+            "step_width": 1.0,
+        }    
+        
+        self.use_obs = False
+        self.state_dim = 10 # (x, y, theta, vx, vy) for robot and human
+        if self.use_obs:
+            self.n_obs_points = 5
+            self.state_dim = 10 + 2 * self.n_obs_points # (x, y, theta, vx, vy) for robot and human + 2 * N closest obstacle points
+             
+        ############### Configurations ################
+        # Normalized action space
+        self.action_scale = np.array([3.0, 3.0])  # Scaling factor to map from [-1, 1] to [-3, 3]
+        self.action_shift = np.array([0.0, 0.0])
+        self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,), dtype=np.float32)
+        
+        # Normalized observation space (x, y, theta, vx, vy) for robot and human
+        self.obs_mean = np.zeros(self.state_dim, dtype=np.float32)
+        self.obs_std = np.array(
+            [self.env_size, self.env_size, np.pi, 3.0, 3.0, # x, y, theta, vx, vy for robot
+             self.env_size, self.env_size, np.pi, 3.0, 3.0] + # x, y, theta, vx, vy for human
+            [self.env_size, self.env_size] * self.n_obs_points, # 2 * N closest obstacle points
+            dtype=np.float32
         )
-
-        # defining observation space
-        # [robot_x, robot_y, robot_theta, human_x, human_y, human_theta]
-        obs_low = np.array(
-            [
-                -self.env_size,
-                -self.env_size,
-                -np.pi,
-                -self.env_size,
-                -self.env_size,
-                -np.pi,
-            ],
-            dtype=np.float32,
-        )
-        obs_high = np.array(
-            [self.env_size, self.env_size, np.pi, self.env_size, self.env_size, np.pi],
-            dtype=np.float32,
-        )
-        self.observation_space = spaces.Box(
-            low=obs_low, high=obs_high, dtype=np.float32
-        )
+        
+        obs_low = -np.ones(self.state_dim, dtype=np.float32)
+        obs_high = np.ones(self.state_dim, dtype=np.float32)
+        self.observation_space = spaces.Box(low=obs_low, high=obs_high, dtype=np.float32)
 
         # Initialize the state of the environment
         self.reset()
+        
+    def normalize_observation(self, state):
+        return (state - self.obs_mean) / self.obs_std
+    
+    def denormalize_observation(self, state):
+        return (state * self.obs_std) + self.obs_mean
+
+    def rescale_action(self, action):
+        # Use a tanh squashing function to ensure actions stay within valid range
+        action = np.tanh(action)
+        # Rescale the action to the original action range for wheel velocities
+        return action * self.action_scale + self.action_shift
 
     def step(self, action):
 
@@ -83,6 +113,7 @@ class HumanRobotEnv(gym.Env):
         # if self.count > 1 and self.count % 10 == 0:
         #     prev_human_pos = self.human.pos.copy()
         #     check = True
+        scaled_action = self.rescale_action(action)
         
         prev_human_pos = self.human.pos.copy()
         prev_robot_pos = self.robot.pos.copy()
@@ -97,7 +128,7 @@ class HumanRobotEnv(gym.Env):
         prev_human_state = np.concatenate([prev_human_pos, [perv_human_theta], [prev_human_vx, prev_human_vy]]).astype(np.float32)
 
         # Update robot position and orientation based on action
-        self.robot.update(action)
+        self.robot.update(scaled_action)
 
         # Update human position and orientation
         self.human.update()
@@ -172,12 +203,24 @@ class HumanRobotEnv(gym.Env):
             'velocity_reward': velocity_reward,
         }
 
-        state = np.concatenate(
-            [robot_pos, [self.robot.theta], human_pos, [self.human.theta]]
-        ).astype(np.float32)
+        if self.use_obs:
+            # N closest obstacle points, should returen an array of Nx2
+            closest_obstacle_points = get_obstacle_points(robot_pos, obstacles, N=self.n_obs_points)
+            state = np.concatenate([
+                robot_pos, [robot_theta], [robot_vx, robot_vy], 
+                human_pos, [human_theta], [human_vx, human_vy],
+                closest_obstacle_points.flatten()
+            ]).astype(np.float32)
+        else:
+            state = np.concatenate([
+                robot_pos, [robot_theta], [robot_vx, robot_vy], 
+                human_pos, [human_theta], [human_vx, human_vy]
+            ]).astype(np.float32)
+        
+        normalized_state = self.normalize_observation(state)
 
-        # self.count += 1
-        return state, reward, terminated, truncated, info
+        print("update normalized_state: ", normalized_state)
+        return normalized_state, reward, terminated, truncated, info
 
     def reset(self, seed=None, options=None):
         # define reset function
@@ -185,22 +228,50 @@ class HumanRobotEnv(gym.Env):
             np.random.seed(seed)
 
         self.count = 0
-        # self.human = Human()
+        
+        # Initialize the human and robot
         self.human = Human_SF(self.obs)
+        
+        human_pos = self.human.pos
+        human_theta = self.human.theta
+        human_vx, human_vy = self.human.get_velocities()
+        human_goal_pos = self.human.get_goal_pos()
+        obstacles = self.human.get_obstacles()
+        init_human_state = np.concatenate([human_pos, [human_theta], [human_vx, human_vy], human_goal_pos]).astype(np.float32)
 
-        # self.robot = Robot(obj_lenght=self.desired_distance, human_pos=self.human.pos)
-        self.robot = RobotDiff(self.robot_config, human_pos=self.human.pos)
+        self.robot = RobotDiff(self.robot_config, init_human_state)
+        
+        robot_pos = self.robot.pos
+        robot_theta = self.robot.theta
+        robot_vx, robot_vy = self.robot.get_velocities()
+        init_robot_state = np.concatenate([robot_pos, [robot_theta], [robot_vx, robot_vy]]).astype(np.float32)
+        
+        # Initialize the reward calculator
         self.reward = RewardCalculator(self.desired_distance, self.agent_radius, self.obs)
 
         self.robot_pos_history = []
         self.human_pos_history = []
-
-        state = np.concatenate(
-            [self.robot.pos, [self.robot.theta], self.human.pos, [self.human.theta]]
-        ).astype(np.float32)
-
-        info = {}
-        return state, info
+        
+        # initialize state as an array of shape self.state_dim
+        # if use_obs is True, the state should include the N(self.n_obs_points) closest obstacle points
+        # else it should just be robot_pos, robot_theta, robot_vx, robot_vy, human_pos, human_theta, human_vx, human_vy
+        if self.use_obs:
+            closest_obstacle_points = get_obstacle_points(robot_pos, self.obstacles, self.n_obs_points)
+            state = np.concatenate([
+                robot_pos, [robot_theta], [robot_vx, robot_vy], 
+                human_pos, [human_theta], [human_vx, human_vy],
+                closest_obstacle_points.flatten()
+            ]).astype(np.float32)
+        else:
+            state = np.concatenate([
+                robot_pos, [robot_theta], [robot_vx, robot_vy], 
+                human_pos, [human_theta], [human_vx, human_vy]
+            ]).astype(np.float32)
+        
+        normalized_state = self.normalize_observation(state)
+        print("reset normalized_state: ", normalized_state)   
+        info = {'state_dim': normalized_state.shape}
+        return normalized_state, info
 
     def render(self, mode="human"):
         plt.clf()
